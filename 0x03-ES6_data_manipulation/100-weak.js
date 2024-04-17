db42f3d070ce3df2f210b9908b55e7179eb55e2a
Hazem Magdy <hhazem0010@gmail.com>
2024-04-17 03:08:42 +0200

    update README & fix some issues in advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,10 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 1);
-  } else {
-    const count = weakMap.get(endpoint);
-    if (count >= 5) {
-      throw new Error('Endpoint load is high');
-    } else {
-      weakMap.set(endpoint, count + 1);
-    }
+  const count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count + 1);
+
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
