5babbfe3b4fe7e83d40f3a0fa55015a61eb75e8f
Hazem Magdy <hhazem0010@gmail.com>
2024-06-04 19:45:39 +0300

    Node_JS_basic - tasks 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+
+/**
+ * Count students in a specific field
+ * 
+ * @param {str} path: database .csv file path
+ */
+const countStudents = (path) => {
+  if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = fs.readFileSync(path, 'utf-8').trim().split('\n');
+  const studentNames = lines.shift().split(',').slice(0);
+
+  const studentGroups = lines.reduce((groups, line) => {
+    const record = line.split(',');
+    const values = record.slice(0);
+    const field = record[record.length - 1];
+
+    groups[field] = groups[field] || [];
+
+    groups[field].push(Object.fromEntries(studentNames.map((
+      name, index) => [name, values[index]])));
+    return groups;
+  }, {});
+
+  // eslint-disable-next-line max-len
+  const totalStudents = Object.values(studentGroups).reduce((previous, current) => previous + current.length, 0);
+  console.log(`Number of students: ${totalStudents}`);
+  
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const namesList = group.map((student) => student.firstname).join(', ');
+    
+    // eslint-disable-next-line max-len
+    console.log(`Number of students in ${field}: ${group.length}, List: ${namesList}`);
+  }
+};
+
+module.exports = countStudents;
