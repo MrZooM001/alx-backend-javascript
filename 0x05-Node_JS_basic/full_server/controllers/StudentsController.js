c5e9f932d6183d451ad0f5e616bd9a3023ebf557
Hazem Magdy <hhazem0010@gmail.com>
2024-06-07 01:35:29 +0300

    NodeJS Basics - fix task 8

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,46 +5,50 @@ const allowedFields = ['CS', 'SWE'];
 class StudentsController {
   static getAllStudents(request, response) {
     const path = process.argv.length > 2 ? process.argv[2] : '';
-    readDatabase(path)
-      .then((studentGroups) => {
-        const resultParts = ['This is the list of our students'];
 
-        // eslint-disable-next-line max-len
-        const sortedFields = Object.entries(studentGroups).sort(([fieldA], [fieldB]) => fieldA.toLowerCase().localeCompare(fieldB.toLowerCase()));
-
-        for (const [field, group] of sortedFields) {
-          const studentNames = group.map((student) => student.firstname).join(', ');
-          resultParts.push(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    readDatabase(path).then((studentGroups) => {
+      const responseParts = ['This is the list of our students'];
+      const storedFields = (fieldA, fieldB) => {
+        if (fieldA[0].toLowerCase() < fieldB[0].toLowerCase()) {
+          return -1;
         }
-        response.status(200).send(resultParts.join('\n'));
-      })
-      .catch((err) => {
-        response.status(500).send(`Cannot load the database: ${err.message}`);
-      });
+        if (fieldA[0].toLowerCase() > fieldB[0].toLowerCase()) {
+          return 1;
+        }
+        return 0;
+      };
+
+      for (const [field, group] of Object.entries(studentGroups).sort(storedFields)) {
+        responseParts.push([
+          `Number of students in ${field}: ${group.length}.`,
+          'List:',
+          group.map((student) => student.firstname).join(', '),
+        ].join(' '));
+      }
+      response.status(200).send(responseParts.join('\n'));
+    }).catch((err) => {
+      response.status(500).send(err instanceof Error ? err.message : err.toString());
+    });
   }
 
   static getAllStudentsByMajor(request, response) {
     const path = process.argv.length > 2 ? process.argv[2] : '';
     const { major } = request.params;
-
     if (!allowedFields.includes(major)) {
-      response.status(400).send('Major parameter must be CS or SWE');
+      response.status(500).send('Major parameter must be CS or SWE');
       return;
     }
 
-    readDatabase(path)
-      .then((studentGroups) => {
-        if (!studentGroups[major]) {
-          response.status(200).send('List: ');
-          return;
-        }
-
-        const studentNames = studentGroups[major].map((student) => student.firstname).join(', ');
-        response.status(200).send(`List: ${studentNames}`);
-      })
-      .catch((err) => {
-        response.status(500).send(`Cannot load the database: ${err.message}`);
-      });
+    readDatabase(path).then((studentGroups) => {
+      let text = '';
+      if (Object.keys(studentGroups).includes(major)) {
+        const group = studentGroups[major];
+        text = `List: ${group.map((student) => student.firstname).join(', ')}`;
+      }
+      response.status(200).send(text);
+    }).catch((err) => {
+      response.status(500).send(err instanceof Error ? err.message : err.toString());
+    });
   }
 }
 
