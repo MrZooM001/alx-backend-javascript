e180c4adcdf76ea08149312aa891d9d2e1d21e1c
Hazem Magdy <hhazem0010@gmail.com>
2024-06-07 00:56:57 +0300

    NodeJS Basics - fix2 task~3

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,31 @@
-const http = require("http");
-const fs = require("fs");
-
+const http = require('http');
+const fs = require('fs');
 
 const app = http.createServer();
-const dbFile = process.argv.length > 2 ? process.argv[2] : "";
+const dbFile = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (path) => new Promise((resolve, reject) => {
   if (!path) {
-    reject(new Error("Cannot load the database"));
+    reject(new Error('Cannot load the database'));
   }
 
   if (path) {
     fs.readFile(path, (error, data) => {
       if (error) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
         const resultParts = [];
         const studentGroups = {};
-        const lines = data.toString("utf-8").trim().split("\n");
-        const dbField = lines[0].split(",");
+        const lines = data.toString('utf-8').trim().split('\n');
+        const dbField = lines[0].split(',');
         const studentNames = dbField.slice(0, dbField.length - 1);
 
         for (const line of lines.slice(1)) {
-          if (line.trim() === "") continue;
+          // eslint-disable-next-line no-continue
+          if (line.trim() === '') continue;
 
-          const record = line.split(",");
+          const record = line.split(',');
           const values = record.slice(0, record.length - 1);
           const field = record[record.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
@@ -42,12 +42,12 @@ const countStudents = (path) => new Promise((resolve, reject) => {
         for (const [field, group] of Object.entries(studentGroups)) {
           resultParts.push([
             `Number of students in ${field}: ${group.length}.`,
-            "List:",
-            group.map((student) => student.firstname).join(", ")
-          ].join(" "));
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
         }
 
-        resolve(resultParts.join("\n"));
+        resolve(resultParts.join('\n'));
       }
     });
   }
@@ -55,41 +55,41 @@ const countStudents = (path) => new Promise((resolve, reject) => {
 
 const serverRoutes = [
   {
-    route: "/",
+    route: '/',
     handler(_, res) {
-      const text = "Hello Holberton School!";
+      const text = 'Hello Holberton School!\n';
 
-      res.setHeader("Content-Type", "text/plain");
-      res.setHeader("Content-Length", text.length);
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', text.length);
       res.statusCode = 200;
       res.write(Buffer.from(text));
     },
   },
   {
-    route: "/students",
+    route: '/students',
     handler(_, res) {
-      const response = ["This is the list of our students"];
+      const response = ['This is the list of our students'];
 
       countStudents(dbFile).then((report) => {
         response.push(report);
-        const text = response.join("\n");
-        res.setHeader("Content-Type", "text/plain");
-        res.setHeader("Content-Length", text.length);
+        const text = response.join('\n');
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', text.length);
         res.statusCode = 200;
         res.write(Buffer.from(text));
       }).catch((err) => {
         response.push(err instanceof Error ? err.message : err.toString());
-        const text = response.join("\n");
-        res.setHeader("Content-Type", "text/plain");
-        res.setHeader("Content-Length", text.length);
+        const text = response.join('\n');
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', text.length);
         res.statusCode = 200;
         res.write(Buffer.from(text));
       });
     },
-  }
+  },
 ];
 
-app.on("request", (req, res) => {
+app.on('request', (req, res) => {
   for (const routeHandler of serverRoutes) {
     if (routeHandler.route === req.url) {
       routeHandler.handler(req, res);
@@ -98,7 +98,7 @@ app.on("request", (req, res) => {
   }
 });
 
-app.listen(1245, "localhost", () => {
+app.listen(1245, 'localhost', () => {
   process.stdout.write('small HTTP server is listening on: http://localhost:1245');
 });
 
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const fs = require("fs");
-
+const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 1245;
-const dbFile = process.argv.length > 2 ? process.argv[2] : "";
+const dbFile = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (path) => new Promise((resolve, reject) => {
   if (!path) {
-    reject(new Error("Cannot load the database"));
+    reject(new Error('Cannot load the database'));
   }
 
   if (path) {
     fs.readFile(path, (error, data) => {
       if (error) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
         const resultParts = [];
         const studentGroups = {};
-        const lines = data.toString("utf-8").trim().split("\n");
-        const dbField = lines[0].split(",");
+        const lines = data.toString('utf-8').trim().split('\n');
+        const dbField = lines[0].split(',');
         const studentNames = dbField.slice(0, dbField.length - 1);
 
         for (const line of lines.slice(1)) {
-          if (line.trim() === "") continue;
+          // eslint-disable-next-line no-continue
+          if (line.trim() === '') continue;
 
-          const record = line.split(",");
+          const record = line.split(',');
           const values = record.slice(0, record.length - 1);
           const field = record[record.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
@@ -43,36 +43,36 @@ const countStudents = (path) => new Promise((resolve, reject) => {
         for (const [field, group] of Object.entries(studentGroups)) {
           resultParts.push([
             `Number of students in ${field}: ${group.length}.`,
-            "List:",
-            group.map((student) => student.firstname).join(", ")
-          ].join(" "));
+            'List:',
+            group.map((student) => student.firstname).join(', '),
+          ].join(' '));
         }
 
-        resolve(resultParts.join("\n"));
+        resolve(resultParts.join('\n'));
       }
     });
   }
 });
 
-app.get("/", (_, res) => {
-  res.send("Hello Holberton School!");
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.get("/students", (_, res) => {
-  const response = ["This is the list of our students"];
+app.get('/students', (_, res) => {
+  const response = ['This is the list of our students'];
 
   countStudents(dbFile).then((report) => {
     response.push(report);
-    const text = response.join("\n");
-    res.setHeader("Content-Type", "text/plain");
-    res.setHeader("Content-Length", text.length);
+    const text = response.join('\n');
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', text.length);
     res.statusCode = 200;
     res.write(Buffer.from(text));
   }).catch((err) => {
     response.push(err instanceof Error ? err.message : err.toString());
-    const text = response.join("\n");
-    res.setHeader("Content-Type", "text/plain");
-    res.setHeader("Content-Length", text.length);
+    const text = response.join('\n');
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', text.length);
     res.statusCode = 200;
     res.write(Buffer.from(text));
   });
